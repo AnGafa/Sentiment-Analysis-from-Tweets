8bc72233907fb0789e3304bcbb37ac8aebb7a622
AnGafa <andreagafa@gmail.com>
2022-10-25 23:18:13 +0200

    sql script

---
+++ sql/SQLQuery1.sql
@@ -0,0 +1,19 @@
+use SentimentAnalysis
+go
+
+drop table dbo.Twitter
+drop table dbo.Users
+
+create table Users
+(
+	UserId uniqueidentifier primary key default newid(),
+	TwitterId numeric(10,0) not null
+)
+
+create table Twitter
+(
+	TweetId uniqueidentifier primary key default newsequentialid(),
+	UserKey uniqueidentifier not null
+		constraint twter_userkey_fk references Users(UserId),
+	Tweet nvarchar(max) not null
+)
