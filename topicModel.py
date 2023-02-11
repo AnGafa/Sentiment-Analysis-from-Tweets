2e9d2ef238be356abaffda05694ce87141d01fd1
AnGafa <andreagafa@gmail.com>
2023-02-11 11:04:58 +0100

    user grouping adjusted to use soft cosine similarity, code cleanup

--- topicModel.py
+++ topicModel.py
@@ -12,12 +12,21 @@ from gensim.models import CoherenceModel
 from nltk import PorterStemmer
 import requests
 
-df = pd.read_csv('./results.csv')
+df = pd.read_csv('./results4.csv')
 
-df.insert(4, "topics", "", True)
-df['topics'] = df['topics'].apply(lambda x: [x])
+for i in range(0, len(df)):
+    df['topics'][i] = df['topics'][i].replace("\"", "")
+    df['topics'][i] = df['topics'][i].replace("\\", "")
+    df['topics'][i] = df['topics'][i].replace("'''", "'")
+    df['topics'][i] = df['topics'][i].replace("' ", "'")
+    df['topics'][i] = df['topics'][i].replace("\",", "'")
+    df['topics'][i] = df['topics'][i].replace("'''", "'")
+    df['topics'][i] = df['topics'][i].replace("''", "'")
+    df['topics'][i] = df['topics'][i].replace("[', ", "[")
+    df['topics'][i] = df['topics'][i].replace("'", "")
+df['topics'] = df['topics'].apply(lambda x: x[1:-1].split(', '))
 
-api_key = "77307d4d-4d53-4f6b-a7b7-ca1363ed9ed7"
+api_key = "5437184f-6f90-45fa-99d5-a8b7c38ea01c"
 url = "https://api.oneai.com/api/v0/pipeline"
 headers = {
   "api-key": api_key, 
@@ -27,32 +36,30 @@ headers = {
 try:
     for i in range(0, len(df)):
         print(i)
-        payload = {
-            "input": df['text'][i],
-            "input_type": "article",
-            "output_type": "json",
-            "multilingual": {
-            "enabled": True
-            },
-            "steps": [
-                {
-                "skill": "article-topics"
+        if(df['topics'][i] == ['']):
+            payload = {
+                "input": df['text'][i],
+                "input_type": "article",
+                "output_type": "json",
+                "multilingual": {
+                "enabled": True
+                },
+                "steps": [
+                    {
+                    "skill": "article-topics"
+                }
+                ],
             }
-            ],
-        }
-        r = requests.post(url, json=payload, headers=headers)
-        data = r.json()
-        for a in data['output']:
-            for b in a['labels']:
-                df['topics'][i].append(b['value'])
-except:
+            r = requests.post(url, json=payload, headers=headers)
+            data = r.json()
+            for a in data['output']:
+                for b in a['labels']:
+                    df['topics'][i].append(b['value'])
+except Exception as e:
+    print(e)
     pass
 
-'''for i in range(0, len(df)):
-    df['topics'][i] = df['topics'][i].replace("'',", "")
-    df['topics'][i] = df['topics'][i].replace("''", "")'''
-
 #hashtag extraction
-df['hashtags'] = df['text'].apply(lambda x: re.findall(r'#(\w+)', x))
+#df['hashtags'] = df['text'].apply(lambda x: re.findall(r'#(\w+)', x))
 
-df.to_csv('./results2.csv', index=False)
+df.to_csv('./results4.csv', index=False)
